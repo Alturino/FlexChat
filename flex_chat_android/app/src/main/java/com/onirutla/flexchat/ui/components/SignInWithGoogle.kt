91b9761438b2cc8e038e1909ae91279cb9aac3f6
Ricky Alturino <alturino001@gmail.com>
2023-12-16 13:41:58 +0700

    add component SignInWithGoogle.kt

---
+++ flex_chat_android/app/src/main/java/com/onirutla/flexchat/ui/components/SignInWithGoogle.kt
@@ -0,0 +1,37 @@
+package com.onirutla.flexchat.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.onirutla.flexchat.R
+import com.onirutla.flexchat.ui.theme.FlexChatTheme
+
+@Composable
+fun SignInWithGoogle(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    IconButton(
+        modifier = modifier,
+        onClick = onClick,
+        content = {
+            Icon(
+                painter = painterResource(id = if (isSystemInDarkTheme()) R.drawable.android_dark_rd_si else R.drawable.android_light_rd_si),
+                contentDescription = null,
+                tint = Color.Unspecified,
+            )
+        }
+    )
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+private fun SignInWithGooglePreview() {
+    FlexChatTheme {
+        SignInWithGoogle(onClick = {})
+    }
+}
