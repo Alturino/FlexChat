6be0b985c70e268230a69cbcde3233d127f7a91b
Ricky Alturino <alturino001@gmail.com>
2023-12-16 13:41:32 +0700

    add component GoogleIconButton.kt

---
+++ flex_chat_android/app/src/main/java/com/onirutla/flexchat/ui/components/GoogleIconButton.kt
@@ -0,0 +1,44 @@
+package com.onirutla.flexchat.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.onirutla.flexchat.R
+import com.onirutla.flexchat.ui.theme.FlexChatTheme
+
+@Composable
+fun GoogleIconButton(
+    modifier: Modifier = Modifier,
+    iconSize: Dp,
+    onClick: () -> Unit,
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Icon(
+            modifier = Modifier.size(iconSize),
+            painter = painterResource(id = if (isSystemInDarkTheme()) R.drawable.android_dark_rd_na else R.drawable.android_neutral_rd_na),
+            contentDescription = null,
+            tint = Color.Unspecified,
+        )
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+private fun GoogleIconButtonPreview() {
+    FlexChatTheme {
+        GoogleIconButton(onClick = {}, iconSize = 75.dp)
+    }
+}
